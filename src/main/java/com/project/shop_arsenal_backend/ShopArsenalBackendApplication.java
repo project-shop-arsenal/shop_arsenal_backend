b8c582e670fb154ade059cde777ce32d378bf975
trantheanh3101 <trantheanhk11@gmail.com>
2024-10-03 09:04:02 +0700

    theanh first project

---
+++ src/main/java/com/project/shop_arsenal_backend/ShopArsenalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.shop_arsenal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopArsenalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopArsenalBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/shop_arsenal_backend/ShopArsenalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.shop_arsenal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopArsenalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
